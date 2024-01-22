065afc2ff2d98e3143558eb68a1aaea41485f298
신시원 <ssw4627@gmail.com>
2024-01-22 17:09:19 +0900

    test

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,6 +2,10 @@
   <div>
     <h1>This is an about page</h1>
   </div>
+  <div>
+    <input class="test-container input-box-style-1 line-claret" value="Testing...">
+    <input class="test-container input-box-style-2 line-darkgray" value="Testing...">
+  </div>
 </template>
 
 <style></style>

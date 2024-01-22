cbd9a7d892c0028e464fa3773d79558d12260861
gusckd96 <gusckd96@naver.com>
2024-01-22 16:48:32 +0900

    feat: 버튼 컴포넌트 추가
    - prettier tab size 변경 : 2 -> 4
    
    - ButtonAtom.vue 생성
    	 * Button 관련 css 코드 작성
    	 * Font 관련 css 코드 작성
    
    Resolves #61

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import ButtonAtom from './components/atoms/ButtonAtom.vue'
 </script>
 
 <template>
-  <header>
-    <div class="bg-A805Cream">
-      Secreto Project
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <header>
+        <div class="bg-A805Cream">
+            Secreto Project
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div>
+        <RouterView />
+    </header>
+    <div>
+        <ButtonAtom customClass="button-style1 button-shadow button-claret">label</ButtonAtom><br />
+        <ButtonAtom customClass="button-style2 button-shadow button-cream">label</ButtonAtom><br />
+        <ButtonAtom customClass="button-style3 button-shadow button-white">label</ButtonAtom><br />
+        <ButtonAtom customClass="button-style4 button-shadow button-violet">label</ButtonAtom><br />
+        <ButtonAtom customClass="button-style5 button-claret button-border-violet">label</ButtonAtom
+        ><br />
+        <ButtonAtom customClass="button-style5 button-lightGrey button-border-grey"
+            >label</ButtonAtom
+        ><br />
+        <ButtonAtom customClass="button-style5 button-khaki button-border-violet">label</ButtonAtom
+        ><br />
+        <ButtonAtom customClass="button-style6 button-blue">label</ButtonAtom><br />
     </div>
-  </header>
-  <RouterView />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/atoms/ButtonAtom.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+const { label, customClass } = defineProps(['label', 'customClass'])
+const emit = defineEmits()
+
+type ClickHandler = () => void
+
+const handleClick: ClickHandler = () => {
+    emit('click')
+}
+</script>
+
+<template>
+    <button :class="customClass" @click="handleClick">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped></style>

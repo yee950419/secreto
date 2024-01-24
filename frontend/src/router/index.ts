6a05f9a32165021901947cd0cadc96843be3d3a2
신시원 <ssw4627@gmail.com>
2024-01-24 16:57:24 +0900

    feat: 셀렉트 박스 구
    - 셀렉트 박스 클래스  1개 구현
    - 추가 클래스 제작 예정
    
    Related to S10P12A805-124

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -9,7 +9,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: HomeView
+            component: sswTestVue
         },
         {
             path: '/game',

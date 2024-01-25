99c49f09055f59302b862871805f0adbe1b48d38
신시원 <ssw4627@gmail.com>
2024-01-25 10:13:29 +0900

    feat: Check Box Emit 구현
    Related to S10P12A805-123

--- frontend/src/components/molecules/CheckBox.vue
+++ frontend/src/components/molecules/CheckBox.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import type { Handler } from '@/types/common'
 defineProps(['customClass', 'customId'])
-const emit = defineEmits([])
+const emit = defineEmits(['checkBoxChange'])
+const checkBoxHandler: Handler = () => {
+    emit('checkBoxChange')
+}
 </script>
 
 <template>
     <div class="flex items-center" :class="customClass">
-        <input type="checkbox" :id="customId" />
+        <input @click="checkBoxHandler" type="checkbox" :id="customId" />
         <label :for="customId"><slot></slot></label>
     </div>
 </template>

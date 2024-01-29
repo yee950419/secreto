ea207d78189c749a1ad8998a2f951d45c1613bef
3dlalswl3 <3dlalswl3@naver.com>
2024-01-29 17:39:34 +0900

    feat: 게시판 컨트롤러 구현
    - 테스트코드 에러 수정
    Related to #S10P12A805-68

--- backend/src/test/java/com/pjg/secreto/board/query/service/BoardQueryServiceImplTest.java
+++ backend/src/test/java/com/pjg/secreto/board/query/service/BoardQueryServiceImplTest.java
@@ -1,7 +1,6 @@
 package com.pjg.secreto.board.query.service;
 
 import com.pjg.secreto.board.query.repository.BoardQueryRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,20 +13,12 @@ class BoardQueryServiceImplTest {
     @Autowired
     BoardQueryService boardQueryService;
 
-    @Autowired
-    PostQueryRepository postQueryRepository;
-
-
-
     @Test
     void getBoardBySpecification() {
     }
 
     @Test
     void getPost() {
-        SearchPostResponseDto post = boardQueryService.getPost(1L);
-        Assertions.assertNotNull(post);
-        System.out.println(post.getRoomUser());
     }
 
     @Test

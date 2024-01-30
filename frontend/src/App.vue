71c2e4dfd50c5bf600870859d540ab9d3dd04fd1
crm03008 <crm03008@gmail.com>
2024-01-30 15:17:43 +0900

    feat: Server Sent Event 로직 추가
    - Server Sent Event 로직 추가
    - 불필요한 파일 삭제
    - 헤더, 푸터 개선
    
    Resolves: S10P12A805-145

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,31 +2,43 @@
 import { RouterView } from 'vue-router'
 import HeaderBar from '@/components/organisms/HeaderBar.vue'
 import FooterBar from '@/components/organisms/FooterBar.vue'
+import { ref, watch, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+const headerSeen = ref(false)
+const route = useRoute()
 
-// const handleServerSentEvent = () => {
-//     const eventSource = new EventSource(VITE_API_BASE_URL + '/alarm') // 서버 엔드포인트를 적절히 설정해야 합니다.
+watch(route, () => {
+    if (route.meta.hide === true) {
+        headerSeen.value = false
+    } else {
+        headerSeen.value = true
+    }
+})
 
-//     eventSource.addEventListener('message', (event) => {
-//         console.log('Server Sent Event received:', event.data)
-//         // 여기에서 서버에서 전송된 이벤트를 처리합니다.
-//     })
+const handleServerSentEvent = () => {
+    const eventSource = new EventSource(import.meta.env.VITE_API_BASE_URL + '/alarm') // 서버 엔드포인트를 적절히 설정해야 합니다.
 
-//     eventSource.addEventListener('error', (event) => {
-//         console.error('Server Sent Event error:', event)
-//         // 여기에서 오류 처리를 수행합니다.
-//     })
-// }
+    eventSource.addEventListener('message', (event) => {
+        console.log('Server Sent Event received:', event.data)
+        // 여기에서 서버에서 전송된 이벤트를 처리합니다.
+    })
 
-// onMounted(() => {
-//     handleServerSentEvent()
-// })
+    eventSource.addEventListener('error', (event) => {
+        console.error('Server Sent Event error:', event)
+        // 여기에서 오류 처리를 수행합니다.
+    })
+}
+
+onMounted(() => {
+    handleServerSentEvent()
+})
 </script>
 
 <template>
     <div class="flex flex-1 flex-col w-screen h-screen">
-        <HeaderBar v-if="$route.meta.hide !== true" />
+        <HeaderBar v-if="headerSeen" />
         <RouterView />
-        <FooterBar v-if="$route.meta.hide !== true" />
+        <FooterBar v-if="headerSeen" />
     </div>
 </template>
 

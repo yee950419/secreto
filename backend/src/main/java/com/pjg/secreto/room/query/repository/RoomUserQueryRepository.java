5801ebc3417d69a65e10923732484731443f67e2
3dlalswl3 <3dlalswl3@naver.com>
2024-01-30 10:29:27 +0900

    feat: 게시판 컨트롤러 구현
    - 테스트코드 삭제
    
    Related to #S10P12A805-68

--- backend/src/main/java/com/pjg/secreto/room/query/repository/RoomUserQueryRepository.java
+++ backend/src/main/java/com/pjg/secreto/room/query/repository/RoomUserQueryRepository.java
@@ -1,19 +1,9 @@
 package com.pjg.secreto.room.query.repository;
 
-<<<<<<<<< Temporary merge branch 1
-import com.pjg.secreto.room.common.entity.RoomUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface RoomUserQueryRepository extends JpaRepository<RoomUser, Long>  {
-    Optional<RoomUser> findById(Long id);
-=========
 import com.pjg.secreto.room.common.entity.Room;
 import com.pjg.secreto.room.common.entity.RoomUser;
 import com.pjg.secreto.user.common.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,5 +13,6 @@ public interface RoomUserQueryRepository extends JpaRepository<RoomUser, Long>, 
     RoomUser findRoomUserByRoomAndUser(Room room, User user);
 
     List<RoomUser> findRoomUserByRoom(Room findRoom);
+
     Optional<RoomUser> findById(Long id);
 }

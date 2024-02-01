7699a8090f491361dc74d4c58b12495337e5db36
DESKTOP-64649GT\Master <crm03008@gmail.com>
2024-02-01 20:16:07 +0900

    fix: 젠킨스 에러 해결을 위한 alias 설정
    - 젠킨스 에러 해결을 위한 alias 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -18,7 +18,8 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            'ant-design-vue': path.resolve(__dirname, 'node_modules/ant-design-vue')
         }
     },
     build: {

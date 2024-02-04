45759da58d198bfab5f133e61a4ce65cf530f0c5
gusckd96 <gusckd96@naver.com>
2024-02-04 19:58:49 +0900

    feat: 게시글 작성 페이지 구현 중
    Related to S10P12A805-172

--- frontend/src/components/pages/BoardView.vue
+++ frontend/src/components/pages/BoardView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import BoardList from '@/components/template/board/BoardList.vue'
 import BoardDetail from '@/components/template/board/BoardDetail.vue'
+import BoardWrite from '../template/board/BoardWrite.vue'
 </script>
 
 <template>
     <div class="flex flex-1 justify-center">
-        <BoardList />
-        <!-- <BoardDetail /> -->
+        <!-- <BoardList />
+        <BoardDetail /> -->
+        <BoardWrite />
     </div>
 </template>
 
---
+++ frontend/src/components/template/board/BoardWrite.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import ButtonAtom from '@/components/atoms/ButtonAtom.vue'
+import BoardWriteEditor from '@/components/molecules/board/BoardWriteEditor.vue'
+import CheckBox from '@/components/molecules/common/CheckBox.vue'
+import InputBox from '@/components/molecules/common/InputBox.vue'
+import SelectBox from '@/components/molecules/common/SelectBox.vue'
+import { ref, type Ref } from 'vue'
+import type { BoardWriteRequestType } from '@/types/board'
+
+const boardWriteRequest: Ref<BoardWriteRequestType> = ref({
+    boardNo: 0,
+    roomUserNo: 0,
+    title: '',
+    content: '',
+    boardCategory: '',
+    publicYn: true,
+    missionCategory: ''
+})
+
+const setContent = (data: string) => {
+    boardWriteRequest.value.content = data
+}
+const submitButtonHandle = () => {
+    alert(boardWriteRequest.value.title)
+    alert(boardWriteRequest.value.content)
+}
+</script>
+
+<template>
+    <div
+        class="flex flex-col w-full md:min-w-[768px] max-w-[1080px] max-md:min-w-0 items-center px-[20px] gap-[10px]"
+    >
+        <div class="w-full flex flex-col gap-[10px] my-[20px]">
+            <div class="flex flex-col gap-[15px] border border-A805LightGrey p-5 rounded-sm">
+                <div class="flex justify-between h-[30px]">
+                    <SelectBox
+                        class="w-[70%]"
+                        :options="[{ label: '미션을 선택해 주세요.', value: 0 }]"
+                    />
+                    <CheckBox class="w-[20%] gap-[10px] justify-end" custom-id="publicYn"
+                        >인증 글 공개 여부</CheckBox
+                    >
+                </div>
+                <InputBox
+                    type="text"
+                    label-class="ps-[10px]"
+                    input-class="input-box-style-2 line-lightGrey bg-A805White text-A805Black"
+                    custom-class="w-full"
+                    custom-id="email"
+                    place-holder="제목을 입력해 주세요."
+                    v-model="boardWriteRequest.title"
+                ></InputBox>
+            </div>
+            <BoardWriteEditor :data="boardWriteRequest.content" @setContent="setContent" />
+            <div class="flex justify-end">
+                <ButtonAtom
+                    custom-class="button-style-4 button-claret text-[18px] font-bold flex justify-center items-center gap-[5px] max-md:w-full"
+                    @button-click="submitButtonHandle"
+                    >등록</ButtonAtom
+                >
+            </div>
+        </div>
+    </div>
+</template>

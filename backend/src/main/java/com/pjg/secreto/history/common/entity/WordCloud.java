9c6c28d063cdac13b5c17de59ac138672883a736
조승우 <kjsw12@naver.com>
2024-02-05 17:08:48 +0900

    feature: 유저 히스토리 정보 일부 구현 - 워드 클라우드 작성 기능 작성
    Related to S10P12A805-201

--- backend/src/main/java/com/pjg/secreto/history/common/entity/WordCloud.java
+++ backend/src/main/java/com/pjg/secreto/history/common/entity/WordCloud.java
@@ -4,21 +4,33 @@ package com.pjg.secreto.history.common.entity;
 import com.pjg.secreto.room.common.entity.Room;
 import com.pjg.secreto.room.common.entity.RoomUser;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tbl_word_cloud")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class WordCloud {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "room_user_no")
     private RoomUser user;
 
     private String content;
 
     private Long value;
+
+    @Builder
+    public WordCloud(RoomUser user, String content, Long value) {
+        this.user = user;
+        this.content = content;
+        this.value = value;
+    }
 }

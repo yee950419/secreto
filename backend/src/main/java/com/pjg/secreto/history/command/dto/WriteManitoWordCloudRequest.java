88473b0cebab444106326bec3c5877ed0f9f9013
조승우 <kjsw12@naver.com>
2024-02-05 17:17:07 +0900

    Revert "Merge branch 'feature/201-backend-히스토리-기능-구현' into 'backend-develop'"
    This reverts merge request !155

--- backend/src/main/java/com/pjg/secreto/history/command/dto/WriteManitoWordCloudRequest.java
+++ backend/src/main/java/com/pjg/secreto/history/command/dto/WriteManitoWordCloudRequest.java
@@ -1,12 +1,4 @@
 package com.pjg.secreto.history.command.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class WriteManitoWordCloudRequest {
-    private String contents;
 }

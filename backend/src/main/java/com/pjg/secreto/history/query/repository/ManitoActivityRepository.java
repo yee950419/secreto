e0daacfa1ac3aa0f92421e999b92914dc17b0722
조승우 <kjsw12@naver.com>
2024-02-06 23:32:43 +0900

    feature: 히스토리 구현
    - 내 마니또 기록(마니또로서), 나의 기록 구현(마니띠로서) 구현
    - 통계 쿼리 일부 수정
    - 마니또 결정 기록 구현
    
    Related to S10P12A805-201

---
+++ backend/src/main/java/com/pjg/secreto/history/query/repository/ManitoActivityRepository.java
@@ -0,0 +1,43 @@
+package com.pjg.secreto.history.query.repository;
+
+
+import com.pjg.secreto.board.common.entity.BoardCategory;
+import com.pjg.secreto.board.common.entity.QBoard;
+import com.pjg.secreto.history.common.entity.QMatching;
+import com.pjg.secreto.history.query.dto.PostDto;
+import com.pjg.secreto.history.query.dto.QPostDto;
+import com.pjg.secreto.room.common.entity.QRoom;
+import com.pjg.secreto.room.common.entity.QRoomUser;
+import com.pjg.secreto.room.common.entity.RoomUser;
+import com.pjg.secreto.user.common.entity.QUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ManitoActivityRepository {
+    private final JPAQueryFactory query;
+
+    public List<PostDto> getBoardActivity(Long roomId, List<RoomUser> users, BoardCategory category){
+        QMatching matching = QMatching.matching;
+        QBoard board = QBoard.board;
+        QRoomUser roomUser = QRoomUser.roomUser;
+        QUser user = QUser.user;
+        QRoom room = QRoom.room;
+
+        List<PostDto> result = query.select(new QPostDto(board))
+                .from(board)
+                .leftJoin(board.roomUser, roomUser).fetchJoin()
+                .leftJoin(board.roomUser.user, user).fetchJoin()
+                .where(board.roomUser.room.id.eq(roomId))
+                .where(board.roomUser.in(users))
+                .where(board.boardCategory.eq(category))
+                .where(board.registerAt.before(board.roomUser.room.roomEndAt))
+                .fetch();
+
+        return result;
+    }
+}

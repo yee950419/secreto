dea67049a8926deb790429b3c7ff2beae9df96a1
신시원 <ssw4627@gmail.com>
2024-02-06 10:09:17 +0900

    feat:  돌발 미션 추가, 예약 리스트 추가

---
+++ frontend/src/components/molecules/game/ExpectedMission.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { CloseSquareOutlined } from '@ant-design/icons-vue'
+</script>
+
+<template>
+    <div class="flex justify-between items-center p-4 text-[16pt] overflow-hidden">
+        <li>
+            <slot></slot>
+        </li>
+        <CloseSquareOutlined />
+    </div>
+</template>
+
+<style scoped></style>

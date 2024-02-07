df078269b4c88fff1e348b1524215117e997a496
조승우 <kjsw12@naver.com>
2024-02-07 09:57:28 +0900

    Merge branch 'feature/alarm' into 'backend-develop'
    fix:  accept user 번호 번환
    
    See merge request s10-webmobile1-sub2/S10P12A805!192

--- backend/src/test/java/com/pjg/secreto/history/query/repository/StaticRepositoryTest.java
+++ backend/src/test/java/com/pjg/secreto/history/query/repository/StaticRepositoryTest.java
@@ -1,37 +1,37 @@
-package com.pjg.secreto.history.query.repository;
-
-import com.pjg.secreto.history.query.dto.SummaryResultData;
-import com.querydsl.core.Tuple;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class StaticRepositoryTest {
-    @Autowired
-    ManitoExpectRepository manitoExpectRepository;
-
-    @Autowired
-    StaticRepository staticRepository;
-
-    @Test
-    void test1(){
-        SummaryResultData mostLikedPost = staticRepository.getMostLikedPost(1L);
-        System.out.println(mostLikedPost);
-    }
-
-    @Test
-    void test2(){
-        SummaryResultData mostWroteCerticationUser = staticRepository.getMostWroteCerticationUser(1L);
-        System.out.println(mostWroteCerticationUser);
-    }
-    @Test
-    void test3(){
-        SummaryResultData fastestCorrectManito = manitoExpectRepository.getFastestCorrectManito(1L);
-        System.out.println(fastestCorrectManito);
-    }
-}
+//package com.pjg.secreto.history.query.repository;
+//
+//import com.pjg.secreto.history.query.dto.SummaryResultData;
+//import com.querydsl.core.Tuple;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class StaticRepositoryTest {
+//    @Autowired
+//    ManitoExpectRepository manitoExpectRepository;
+//
+//    @Autowired
+//    StaticRepository staticRepository;
+//
+//    @Test
+//    void test1(){
+//        SummaryResultData mostLikedPost = staticRepository.getMostLikedPost(1L);
+//        System.out.println(mostLikedPost);
+//    }
+//
+//    @Test
+//    void test2(){
+//        SummaryResultData mostWroteCerticationUser = staticRepository.getMostWroteCerticationUser(1L);
+//        System.out.println(mostWroteCerticationUser);
+//    }
+//    @Test
+//    void test3(){
+//        SummaryResultData fastestCorrectManito = manitoExpectRepository.getFastestCorrectManito(1L);
+//        System.out.println(fastestCorrectManito);
+//    }
+//}

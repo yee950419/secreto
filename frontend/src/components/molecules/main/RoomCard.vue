74320b6d354d2aedf3b4361109d5f2e583339b38
DESKTOP-64649GT\Master <crm03008@gmail.com>
2024-02-09 15:52:48 +0900

    fix: 종료된 방이 종료라고 표시되지 않던 문제 해결
    - 종료 상태 조건문을 앞으로 두어 해결

--- frontend/src/components/molecules/main/RoomCard.vue
+++ frontend/src/components/molecules/main/RoomCard.vue
@@ -27,41 +27,24 @@ const deleteButtonClick: Handler = () => {
 
 <template>
     <div
-        class="bg-A805Cream w-[45%] aspect-[5/3] rounded-lg shadow-rb cursor-pointer relative hover:scale-[102%] transition ease-in-out"
-    >
+        class="bg-A805Cream w-[45%] aspect-[5/3] rounded-lg shadow-rb cursor-pointer relative hover:scale-[102%] transition ease-in-out">
         <div class="absolute top-[2px] left-[2px] flex gap-[5px] items-center">
-            <BadgeAtom
-                custom-class="text-[1rem] bg-A805Purple text-A805White m-1 py-[1px] w-[60px] text-center"
-                v-if="roomInfo.standbyYn"
-                >입장전</BadgeAtom
-            >
-            <BadgeAtom
-                custom-class="text-[1rem] bg-A805Khaki text-A805White m-1 py-[1px] w-[60px] text-center "
-                v-else-if="!roomInfo.roomStartAt || new Date(roomInfo.roomStartAt) > new Date()"
-                >시작전</BadgeAtom
-            >
-            <BadgeAtom
-                custom-class="text-[1rem] bg-A805DarkGrey text-A805White m-1 py-[1px] w-[60px] text-center"
-                v-else-if="
-                    roomInfo.roomStatus === 'END' || new Date(roomInfo.roomEndAt) < new Date()
-                "
-                >종료</BadgeAtom
-            >
-            <BadgeAtom
-                custom-class="text-[1rem] bg-A805Red text-A805White m-1 py-[1px] w-[60px] text-center"
-                v-else
-                >참여중</BadgeAtom
-            >
+            <BadgeAtom custom-class="text-[1rem] bg-A805Purple text-A805White m-1 py-[1px] w-[60px] text-center"
+                v-if="roomInfo.standbyYn">입장전</BadgeAtom>
+            <BadgeAtom custom-class="text-[1rem] bg-A805DarkGrey text-A805White m-1 py-[1px] w-[60px] text-center"
+                v-else-if="roomInfo.roomStatus === 'END'
+                    ">종료</BadgeAtom>
+            <BadgeAtom custom-class="text-[1rem] bg-A805Khaki text-A805White m-1 py-[1px] w-[60px] text-center "
+                v-else-if="!roomInfo.roomStartAt || new Date(roomInfo.roomStartAt) > new Date()">시작전</BadgeAtom>
+
+            <BadgeAtom custom-class="text-[1rem] bg-A805Red text-A805White m-1 py-[1px] w-[60px] text-center" v-else>참여중
+            </BadgeAtom>
 
             <TextAtom custom-class="text-[1.2rem]">{{ $props.roomInfo.participantCnt }}명</TextAtom>
             <div class="flex items-center">
-                <img
-                    v-if="isMaster"
+                <img v-if="isMaster"
                     src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People/Woman%20Fairy.png"
-                    alt="Woman Fairy"
-                    width="35"
-                    height="35"
-                />
+                    alt="Woman Fairy" width="35" height="35" />
             </div>
         </div>
 
@@ -73,18 +56,14 @@ const deleteButtonClick: Handler = () => {
                 {{ props.roomInfo.nickname }}
             </TextAtom>
         </div>
-        <ButtonAtom
-            class="absolute bottom-0 right-2 text-[20px] hover:scale-[125%] transition ease-in-out"
-            @click.stop="likeButtonClick"
-        >
+        <ButtonAtom class="absolute bottom-0 right-2 text-[20px] hover:scale-[125%] transition ease-in-out"
+            @click.stop="likeButtonClick">
             <TextAtom class="text-A805Black" v-show="!roomInfo.bookmarkYn">♡</TextAtom>
-            <TextAtom class="text-A805Red" v-show="roomInfo.bookmarkYn">♥</TextAtom></ButtonAtom
-        >
+            <TextAtom class="text-A805Red" v-show="roomInfo.bookmarkYn">♥</TextAtom>
+        </ButtonAtom>
         <ButtonAtom
             class="absolute top-[-10px] right-[-3px] font-bold text-[16px] hover:scale-[125%] hover:text-A805Red transition ease-in-out"
-            @click.stop="deleteButtonClick"
-            >✕</ButtonAtom
-        >
+            @click.stop="deleteButtonClick">✕</ButtonAtom>
     </div>
 </template>
 

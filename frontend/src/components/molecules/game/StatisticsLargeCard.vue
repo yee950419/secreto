fcc8eff83fa0716203754e655826835f7025dfa5
gusckd96 <gusckd96@naver.com>
2024-02-12 02:46:48 +0900

    feat: 게임 통계 페이지 화면 구현
    Related to S10P12A805-245

---
+++ frontend/src/components/molecules/game/StatisticsLargeCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import TextAtom from '@/components/atoms/TextAtom.vue'
+import defaultImage from '@/assets/images/character2.png'
+defineProps({
+    title: { type: String, required: true },
+    content: { type: Object as () => Array<String>, required: true },
+    imageUrl: { type: String as () => string | null }
+})
+</script>
+
+<template>
+    <div
+        class="flex flex-col p-3 md:max-w-[400px] w-full border border-A805DarkGrey rounded-lg shadow-rb gap-3 items-center justify-between"
+    >
+        <TextAtom class="text-[20px] font-bold text-center">{{ title }}</TextAtom>
+        <img
+            :src="imageUrl ? imageUrl : defaultImage"
+            class="w-full rounded-xl bg-A805LightGrey h-[130px]"
+            :class="imageUrl ? 'object-cover' : 'object-contain'"
+        />
+        <div class="flex flex-col text-[22px] w-full items-center gap-1">
+            <TextAtom class="text-A805Black">{{ content[0] }}</TextAtom>
+            <div class="flex w-full justify-around">
+                <TextAtom class="text-[16px] text-A805DarkGrey">{{ content[1] }}</TextAtom>
+                <TextAtom class="text-[16px] text-A805Khaki font-bold">{{ content[2] }}</TextAtom>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>

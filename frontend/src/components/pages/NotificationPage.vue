4694c790e441b0cc0e3c94e982ed163b796a5abe
gusckd96 <gusckd96@naver.com>
2024-02-15 09:20:35 +0900

    feat: 게시글 알림 클릭 시 리다이렉트

--- frontend/src/components/pages/NotificationPage.vue
+++ frontend/src/components/pages/NotificationPage.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TextAtom from '@/components/atoms/TextAtom.vue';
-import type { notificationTypes } from '@/types/notify';
-import { notificationRead } from '@/api/notification';
-import { inject, type Ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import TextAtom from '@/components/atoms/TextAtom.vue'
+import type { notificationTypes } from '@/types/notify'
+import { notificationRead } from '@/api/notification'
+import { inject, type Ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
 const emit = defineEmits(['refreshNotify'])
 
@@ -13,28 +13,47 @@ const notificationLists = inject('notifyLists') as Ref<notificationTypes[]>
 
 const notifyReading = (alarmNo: number) => {
     console.log(alarmNo, Number(route.params.roomNo))
-    notificationRead(alarmNo, Number(route.params.roomNo), ({ data }) => {
-        console.log(data)
-        emit('refreshNotify')
-    }, (error) => {
-        console.log(error)
-    })
+    notificationRead(
+        alarmNo,
+        Number(route.params.roomNo),
+        ({ data }) => {
+            console.log(data)
+            emit('refreshNotify')
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
 }
 
+const notifyClickHandle = (notify: notificationTypes) => {
+    notifyReading(notify.alarmNo)
+    if (!!Number(notify.author) === true) {
+        router.push({
+            name: 'game-board-detail',
+            query: { boardNo: notify.author }
+        })
+    }
+}
 </script>
 
 <template>
     <div class="flex flex-1 flex-col bg-A805White">
         <TextAtom custom-class="text-1">알림 리스트</TextAtom>
-        <template v-for="notify in notificationLists.slice().reverse()" class="gap-[30px]">
-            <div v-if="!notify.readYn"
-                class="flex md:flex-wrap items-center justify-center h-[60px] md:mx-[30px] max-md:mx-[10px] rounded-md border-black border-solid border-2 mt-[30px] bg-A805RealWhite md:gap-[30px] max-md:gap-[10px] px-[10px]"
-                @click="notifyReading(notify.alarmNo)">
+        <template v-for="notify in notificationLists.slice().reverse()" :key="notify.alarmNo">
+            <div
+                v-if="!notify.readYn"
+                class="flex md:flex-wrap items-center justify-center h-[60px] md:mx-[30px] max-md:mx-[10px] rounded-md border-black border-solid border-2 mt-[30px] bg-A805RealWhite md:gap-[30px] max-md:gap-[10px] px-[10px] cursor-pointer"
+                @click="() => notifyClickHandle(notify)"
+            >
                 <TextAtom custom-class="text-A805Red">New!</TextAtom>
                 <TextAtom>{{ notify.content }}</TextAtom>
             </div>
-            <div v-else @click="notifyReading(notify.alarmNo)"
-                class="flex  md:flex-wrap items-center justify-center h-[60px] md:mx-[30px] max-md:mx-[10px] rounded-md border-black border-solid border-2 mt-[30px] bg-A805Grey text-A805Cream max-md:gap-[10px] md:gap-[30px] px-[10px]">
+            <div
+                v-else
+                @click="() => notifyClickHandle(notify)"
+                class="flex md:flex-wrap items-center justify-center h-[60px] md:mx-[30px] max-md:mx-[10px] rounded-md border-black border-solid border-2 mt-[30px] bg-A805Grey text-A805Cream max-md:gap-[10px] md:gap-[30px] px-[10px] cursor-pointer"
+            >
                 <TextAtom>{{ notify.content }}</TextAtom>
                 <TextAtom>읽기 완료</TextAtom>
             </div>

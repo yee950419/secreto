c9a33c2aedfb8ace11060135b9c0894c940dd2b4
gusckd96 <gusckd96@naver.com>
2024-02-15 12:46:33 +0900

    design: 알림창 디자인 수정

--- frontend/src/components/organisms/common/NavBar.vue
+++ frontend/src/components/organisms/common/NavBar.vue
@@ -9,6 +9,7 @@ import { useMenuStore } from '@/stores/menu'
 import { useRouter } from 'vue-router'
 import image1 from '@/assets/images/manito-avatar.png'
 import image2 from '@/assets/images/character.png'
+import TextAtom from '@/components/atoms/TextAtom.vue'
 const prop = defineProps({
     roomName: {
         type: String as () => String,
@@ -33,40 +34,46 @@ const notificationLists: Ref<notificationTypes[]> = inject('notifyLists') as Ref
 >
 const roomUserInfo = inject('roomUserInfo') as Ref<RoomUserInfoType>
 const userList = inject('userList') as Ref<userType[]>
-const maniti = ref() as Ref<userType>;
+const maniti = ref() as Ref<userType>
 const showSubMenu = ref<string[]>([])
 const activeMenu = ref(-1)
 const router = useRouter()
 const unReadMessage = ref(0)
 
-watch(maniti, () => {
-    getManiti();
-}, { deep: true });
+watch(
+    maniti,
+    () => {
+        getManiti()
+    },
+    { deep: true }
+)
 
-watch(roomUserInfo, () => {
-    getManiti();
-}, { deep: true });
+watch(
+    roomUserInfo,
+    () => {
+        getManiti()
+    },
+    { deep: true }
+)
 
 const getManiti = () => {
     userList.value.forEach((user) => {
         if (user.usersManito === roomUserInfo.value.roomUserNo) {
-            maniti.value = user;
+            maniti.value = user
         }
-    });
-};
+    })
+}
 
 const makeRoom = (roomName: string) => {
     let imageUrl = ''
 
     if (roomName === '마니또') {
         imageUrl = image1
-    }
-    else if (roomName === '마니띠') {
+    } else if (roomName === '마니띠') {
         getManiti()
-        roomName = maniti.value?.nickname + '(나의 마니띠)' || roomName; // 옵셔널 체이닝 연산자 사용
-        imageUrl = maniti.value?.profileUrl || ''; // 옵셔널 체이닝 연산자 사용
-    }
-    else {
+        roomName = maniti.value?.nickname + '(나의 마니띠)' || roomName // 옵셔널 체이닝 연산자 사용
+        imageUrl = maniti.value?.profileUrl || '' // 옵셔널 체이닝 연산자 사용
+    } else {
         imageUrl = image2
     }
     emit('make-room', {
@@ -94,8 +101,7 @@ watch(
 
 watch(
     () => prop.roomInfo,
-    () => {
-    }
+    () => {}
 )
 
 watch(
@@ -131,60 +137,136 @@ onMounted(() => {
 
 <template>
     <div
-        class="flex flex-col h-full bg-A805White md:min-w-[230px] max-md:w-full md:border-solid md:border-2 md:border-A805Cream md:overflow-y-auto">
-        <MenuItem v-if="roomUserInfo?.roomUserNo === roomInfo?.hostRoomUserNo" custom-class="menu-item"
-            :active="activeMenu === 0" @menu-click="
+        class="flex flex-col h-full bg-A805White md:min-w-[230px] max-md:w-full md:border-solid md:border-2 md:border-A805Cream md:overflow-y-auto"
+    >
+        <MenuItem
+            v-if="roomUserInfo?.roomUserNo === roomInfo?.hostRoomUserNo"
+            custom-class="menu-item"
+            :active="activeMenu === 0"
+            @menu-click="
                 handleMenuClick(0), handleClick(), router.push({ name: 'game-roomsettings' })
-                ">
-        <div class="flex w-full items-center gap-[20px]">
-            방 설정
-            <SettingOutlined />
-        </div>
+            "
+        >
+            <div class="flex w-full items-center gap-[20px]">
+                방 설정
+                <SettingOutlined />
+            </div>
         </MenuItem>
         <MenuItem custom-class="menu-item">{{ roomName }}</MenuItem>
-        <MenuItem custom-class="menu-item" :active="activeMenu === 1" @menu-click="
-            handleMenuClick(1), handleClick(), router.push({ name: 'game-notification' })
-            ">알림 {{ unReadMessage }} 건
-        <span v-if="unReadMessage > 0" class="ml-[20px] text-A805Red">새로운 알림!</span>
+        <MenuItem
+            custom-class="menu-item"
+            :active="activeMenu === 1"
+            @menu-click="
+                handleMenuClick(1), handleClick(), router.push({ name: 'game-notification' })
+            "
+        >
+            <div class="flex items-center gap-2">
+                <span> 알림 </span>
+                <div
+                    class="bg-A805Red w-[30px] h-[30px] flex justify-center items-center rounded-full text-A805White"
+                    v-if="unReadMessage > 0"
+                >
+                    <TextAtom class="">
+                        {{ unReadMessage }}
+                    </TextAtom>
+                </div>
+            </div>
+            <!-- <span v-if="unReadMessage > 0" class="ml-[20px] text-A805Red">●</span> -->
         </MenuItem>
-        <MenuItem v-if="roomInfo?.roomStartYn === true" custom-class="menu-item" :active="activeMenu === 3" @menu-click="
-            handleClick(), router.push({ name: 'game-participate' }), handleMenuClick(3)
-            ">참여 인원</MenuItem>
-        <MenuItem v-if="roomInfo?.roomStartYn === true" custom-class="menu-item" :active="activeMenu === 4"
-            @menu-click="handleMenuClick(4), router.push({ name: 'game-mission' }), handleClick()">내 미션</MenuItem>
-        <MenuItem v-if="roomInfo?.roomStartYn === true" custom-class="menu-item" :active="activeMenu === 5"
-            @menu-click="handleMenuClickAndToggleSubMenu(5, 'chat')">채팅</MenuItem>
+        <MenuItem
+            v-if="roomInfo?.roomStartYn === true"
+            custom-class="menu-item"
+            :active="activeMenu === 3"
+            @menu-click="
+                handleClick(), router.push({ name: 'game-participate' }), handleMenuClick(3)
+            "
+            >참여 인원</MenuItem
+        >
+        <MenuItem
+            v-if="roomInfo?.roomStartYn === true"
+            custom-class="menu-item"
+            :active="activeMenu === 4"
+            @menu-click="handleMenuClick(4), router.push({ name: 'game-mission' }), handleClick()"
+            >내 미션</MenuItem
+        >
+        <MenuItem
+            v-if="roomInfo?.roomStartYn === true"
+            custom-class="menu-item"
+            :active="activeMenu === 5"
+            @menu-click="handleMenuClickAndToggleSubMenu(5, 'chat')"
+            >채팅</MenuItem
+        >
         <div v-if="showSubMenu.includes('chat')">
-            <MenuItem custom-class="sub-menu-item" @menu-click="makeRoom('마니또'), handleClick()">마니또와의 채팅</MenuItem>
-            <MenuItem custom-class="sub-menu-item" @menu-click="makeRoom('마니띠'), handleClick()">마니띠와의 채팅</MenuItem>
-            <MenuItem custom-class="sub-menu-item" @menu-click="makeRoom('단체 채팅'), handleClick()">단체 채팅</MenuItem>
+            <MenuItem custom-class="sub-menu-item" @menu-click="makeRoom('마니또'), handleClick()"
+                >마니또와의 채팅</MenuItem
+            >
+            <MenuItem custom-class="sub-menu-item" @menu-click="makeRoom('마니띠'), handleClick()"
+                >마니띠와의 채팅</MenuItem
+            >
+            <MenuItem
+                custom-class="sub-menu-item"
+                @menu-click="makeRoom('단체 채팅'), handleClick()"
+                >단체 채팅</MenuItem
+            >
         </div>
         <!-- 다른 세부 메뉴들도 추가 가능 -->
-        <MenuItem v-if="roomInfo?.roomStartYn === true" custom-class="menu-item" :active="activeMenu === 6"
-            @menu-click="handleMenuClickAndToggleSubMenu(6, 'board')">게시판</MenuItem>
+        <MenuItem
+            v-if="roomInfo?.roomStartYn === true"
+            custom-class="menu-item"
+            :active="activeMenu === 6"
+            @menu-click="handleMenuClickAndToggleSubMenu(6, 'board')"
+            >게시판</MenuItem
+        >
         <div v-if="showSubMenu.includes('board')">
-            <MenuItem custom-class="sub-menu-item" @menu-click="
-                router.push({ name: 'game-board-list', query: { boardCategory: 'NOTICE' } }),
-                handleClick()
-                ">공지 게시판</MenuItem>
-            <MenuItem custom-class="sub-menu-item" @menu-click="
-                router.push({
-                    name: 'game-board-list',
-                    query: { boardCategory: 'CERTIFICATE' }
-                }),
-                handleClick()
-                ">인증 게시판</MenuItem>
-            <MenuItem custom-class="sub-menu-item" @menu-click="
-                router.push({ name: 'game-board-list', query: { boardCategory: 'BOAST' } }),
-                handleClick()
-                ">자랑 게시판</MenuItem>
+            <MenuItem
+                custom-class="sub-menu-item"
+                @menu-click="
+                    router.push({ name: 'game-board-list', query: { boardCategory: 'NOTICE' } }),
+                        handleClick()
+                "
+                >공지 게시판</MenuItem
+            >
+            <MenuItem
+                custom-class="sub-menu-item"
+                @menu-click="
+                    router.push({
+                        name: 'game-board-list',
+                        query: { boardCategory: 'CERTIFICATE' }
+                    }),
+                        handleClick()
+                "
+                >인증 게시판</MenuItem
+            >
+            <MenuItem
+                custom-class="sub-menu-item"
+                @menu-click="
+                    router.push({ name: 'game-board-list', query: { boardCategory: 'BOAST' } }),
+                        handleClick()
+                "
+                >자랑 게시판</MenuItem
+            >
         </div>
-        <MenuItem v-if="roomInfo?.roomStatus === 'END'" custom-class="menu-item" :active="activeMenu === 7"
-            @menu-click="handleMenuClick(7), handleClick(), router.push({ name: 'game-statistic' })">게임 통계</MenuItem>
-        <MenuItem v-if="roomInfo?.roomStatus === 'END'" custom-class="menu-item" :active="activeMenu === 8"
-            @menu-click="handleMenuClick(8), handleClick(), router.push({ name: 'game-timeline' })">나의 히스토리</MenuItem>
-        <MenuItem v-if="roomInfo?.roomStatus === 'END'" custom-class="menu-item" :active="activeMenu === 9"
-            @menu-click="handleMenuClick(9), handleClick(), router.push({ name: 'game-wordcloud' })">후기</MenuItem>
+        <MenuItem
+            v-if="roomInfo?.roomStatus === 'END'"
+            custom-class="menu-item"
+            :active="activeMenu === 7"
+            @menu-click="handleMenuClick(7), handleClick(), router.push({ name: 'game-statistic' })"
+            >게임 통계</MenuItem
+        >
+        <MenuItem
+            v-if="roomInfo?.roomStatus === 'END'"
+            custom-class="menu-item"
+            :active="activeMenu === 8"
+            @menu-click="handleMenuClick(8), handleClick(), router.push({ name: 'game-timeline' })"
+            >나의 히스토리</MenuItem
+        >
+        <MenuItem
+            v-if="roomInfo?.roomStatus === 'END'"
+            custom-class="menu-item"
+            :active="activeMenu === 9"
+            @menu-click="handleMenuClick(9), handleClick(), router.push({ name: 'game-wordcloud' })"
+            >후기</MenuItem
+        >
     </div>
 </template>
 
--- frontend/src/components/pages/NotificationPage.vue
+++ frontend/src/components/pages/NotificationPage.vue
@@ -5,6 +5,7 @@ import { notificationRead } from '@/api/notification'
 import { inject, type Ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { convertStringToRegistrationDateTime } from '@/utils/date'
+import ButtonAtom from '../atoms/ButtonAtom.vue'
 const emit = defineEmits(['refreshNotify'])
 
 const router = useRouter()
@@ -24,8 +25,11 @@ const notifyReading = (alarmNo: number) => {
     )
 }
 
+const isBoardNotify = (notify: notificationTypes) => {
+    return !!Number(notify.author) === true
+}
 const boardDetailMove = (notify: notificationTypes) => {
-    if (!!Number(notify.author) === true) {
+    if (isBoardNotify(notify)) {
         router.push({
             name: 'game-board-detail',
             query: { boardNo: notify.author }
@@ -35,19 +39,40 @@ const boardDetailMove = (notify: notificationTypes) => {
 </script>
 
 <template>
-    <div class="flex flex-1 flex-col bg-A805White">
-        <TextAtom custom-class="text-1">알림 리스트</TextAtom>
+    <div class="flex flex-1 flex-col bg-A805White p-10">
+        <TextAtom custom-class="text-1 w-full text-center">알림 리스트</TextAtom>
         <template v-for="notify in notificationLists.slice().reverse()" :key="notify.alarmNo">
-            <div v-if="!notify.readYn"
-                class="flex md:flex-wrap items-center justify-center h-[60px] md:mx-[30px] max-md:mx-[10px] rounded-md border-black border-solid border-2 mt-[30px] bg-A805RealWhite md:gap-[30px] max-md:gap-[10px] px-[10px] cursor-pointer"
-                @click="() => notifyReading(notify.alarmNo)">
+            <div
+                v-if="!notify.readYn"
+                class="flex items-center justify-center h-[60px] md:mx-[30px] max-md:mx-[10px] rounded-md border-black border-solid border-2 mt-[30px] bg-A805RealWhite md:gap-[30px] max-md:gap-[10px] px-[10px] cursor-pointer"
+                @click="() => notifyReading(notify.alarmNo)"
+            >
                 <TextAtom custom-class="text-A805Red">New!</TextAtom>
-                <TextAtom>{{ notify.content }} {{ convertStringToRegistrationDateTime(notify.generatedAt) }}</TextAtom>
+                <TextAtom class="font-bold"
+                    >{{ notify.content }}
+                    <span class="text-A805DarkGrey text-[14px]"
+                        >({{ convertStringToRegistrationDateTime(notify.generatedAt) }})</span
+                    ></TextAtom
+                >
             </div>
-            <div v-else @click="() => boardDetailMove(notify)"
-                class="flex md:flex-wrap items-center justify-center h-[60px] md:mx-[30px] max-md:mx-[10px] rounded-md border-black border-solid border-2 mt-[30px] bg-A805Grey text-A805Cream max-md:gap-[10px] md:gap-[30px] px-[10px] cursor-pointer">
-                <TextAtom>{{ notify.content }} {{ convertStringToRegistrationDateTime(notify.generatedAt) }}</TextAtom>
+            <div
+                v-else
+                @click="() => notifyReading(notify.alarmNo)"
+                class="flex items-center justify-center h-[60px] md:mx-[30px] max-md:mx-[10px] rounded-md border-black border-solid border-2 mt-[30px] bg-A805Grey bg-opacity-50 text-A805Black max-md:gap-[10px] md:gap-[30px] px-[10px]"
+            >
+                <TextAtom
+                    >{{ notify.content }}
+                    <span class="text-A805DarkGrey text-[14px]"
+                        >({{ convertStringToRegistrationDateTime(notify.generatedAt) }})</span
+                    ></TextAtom
+                >
                 <TextAtom>읽기 완료</TextAtom>
+                <ButtonAtom
+                    class="text-A805Blue"
+                    v-if="isBoardNotify(notify)"
+                    @click="() => boardDetailMove(notify)"
+                    >게시글 이동</ButtonAtom
+                >
             </div>
         </template>
     </div>
